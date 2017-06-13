92169fdec666635c9376be8c5b091937529943ac
passaKorn-K <jakkajee380@gmail.com>
2017-06-14 00:35:02 +0700

    add entities

--- ReportManagement/ReportManagement/User.cs
+++ ReportManagement/ReportManagement/User.cs
@@ -21,10 +21,12 @@ namespace ReportManagement
             this.Opinions = new HashSet<Opinion>();
         }
     
-        public int userID { get; set; }
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public string position { get; set; }
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Action> Actions { get; set; }
